029859303dfbca0421325cd9dc7555004fb5be62
AUBRY Bastien <38477148+Elevane@users.noreply.github.com>
2023-01-20 08:25:00 +0100

    Update CreateAccount.js

--- src/Components/auth/CreateAccount.js
+++ src/Components/auth/CreateAccount.js
@@ -42,7 +42,7 @@ export default function CreateAccount() {
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h2 className="text-center text-dark mt-5">Map Marker</h2>
+          <h2 className="text-center text-dark mt-5">Complot</h2>
           <div className="text-center mb-5 text-dark">TP PWA bastien AUBRY</div>
           <div className="card my-5">
             <form
